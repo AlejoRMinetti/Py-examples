d350c390a00ed00db97b6ced9979a3d5d4386f4e
Alejo R Minetti <alejorm@gmail.com>
2021-10-24 00:51:36 -0300

    Audio scripts speach to text

---
+++ Audio/speachToText/live_recognizer.py
@@ -0,0 +1,16 @@
+import speech_recognition as sr
+import sys
+
+#read duration from the arguments
+duration = int(sys.argv[1])
+
+# initialize the recognizer
+r = sr.Recognizer()
+print("Please talk")
+with sr.Microphone() as source:
+    # read the audio data from the default microphone
+    audio_data = r.record(source, duration=duration)
+    print("Recognizing...")
+    # convert speech to text
+    text = r.recognize_google(audio_data)
+    print(text)
