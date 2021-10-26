a0912cda074eb5b13d30a81a006025c17ece2db6
Alejo R Minetti <alejorm@gmail.com>
2021-10-26 12:03:33 -0300

    Exceptions try raise

--- Courses/Intermedio - Platzi/debugging.py
+++ Courses/Intermedio - Platzi/debugging.py
@@ -1,15 +1,21 @@
 def divisors(num):
     divisors = []
     for i in range(1, num + 1):
-        if num % i == 1: # corriendo el debug, se ve que == 0
+        if num % i == 0:
             divisors.append(i)
     return divisors
 
 
 def run():
-    num = int(input('Ingresa un número: '))
-    print(divisors(num))
-    print("Terminó mi programa")
+    try:
+        num = int(input('Ingresa un número: '))
+        if num < 0:
+            raise ValueError
+        print(divisors(num))
+        print("Terminó mi programa")
+    except ValueError:
+        print("Debe ser un entero positivo")
+
     
 
 # Usando el debugger encontrar el error
