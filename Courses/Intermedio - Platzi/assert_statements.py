739f5352b1b5a59fe6304d87029a32d427d5b952
Alejo R Minetti <alejorm@gmail.com>
2021-10-26 12:21:27 -0300

    assert statement

---
+++ Courses/Intermedio - Platzi/assert_statements.py
@@ -0,0 +1,18 @@
+def divisors(num):
+    divisors = []
+    for i in range(1, num + 1):
+        if num % i == 0:
+            divisors.append(i)
+    return divisors
+
+
+def run():
+    num = input('Ingresa un número: ')
+    assert num.isnumeric(), "Debes ingresar un número positivo"
+    print(divisors(int(num)))
+    print("Terminó mi programa")
+
+  
+# Usando el debugger encontrar el error
+if __name__ == '__main__':
+    run()
